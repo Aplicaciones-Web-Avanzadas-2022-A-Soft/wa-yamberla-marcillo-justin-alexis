744682c18be30279eb727d8a75b4ff4d922188c6
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-05-17 08:34:39 -0500

    Arreglos y Operadores

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -1,2 +1,48 @@
 //02-arreglos.js
 let arreglo = [6, 7 ,8 ,9, 10];
+arreglo = 1;
+arreglo = true;
+arreglo = undefined;
+arreglo = null;
+arreglo = {};
+arreglo = [true, 1, 1.1, "Justin", 'Alexis', undefined, null, {}, [1, 2]];
+arreglo = [6, 7 ,8 ,9, 10];
+
+//for of: iterar valores
+for (let numero of arreglo){
+    console.log('numero', numero);
+}
+//for in: iterar indices o indices JSON
+for(let indice in arreglo){
+    arreglo[indice];
+    console.log('indice', indice);
+}
+let objetoPrueba = {a: '1', b: '2', c: '3'};
+for (let llave in objetoPrueba){
+    console.log('llave', llave);
+}
+
+//métodos para arreglos
+arreglo.push(11); //Añadir al final de un elemento
+// [6, 7, 8, 9, 10, 11]
+
+arreglo.pop(); //Eliminar al final de un elemento
+// [6, 7, 8, 9, 10]
+
+arreglo.unshift(5); //Añadir al principio de un arreglo
+// [5, 6, 7, 8, 9, 10]
+
+console.log(arreglo);
+
+//splice(indice, numero de elementos eliminado, ...items a agregar)
+// EJ arreglo.splice(0, 3, 1, 2, 3, 4, 5, 6);
+arreglo.splice(0, 0, 4);
+// [4, 5, 6, 7, 8, 9, 10]
+console.log(arreglo);
+
+const indiceNueve = arreglo.indexOf(9); //Encuentra el primer elemento y devuelve el indice0
+arreglo.splice(indiceNueve, 2);
+// [4, 5, 6, 7, 8]
+
+console.log(arreglo);
+
---
+++ 01-javascript/03-operadores.js
@@ -0,0 +1,68 @@
+//03-operados.js
+const arreglo = [
+    {
+        id: 1,
+        nombre: 'Adrian',
+        nota: 5
+    },
+    {
+        id: 2,
+        nombre: 'Vicente',
+        nota: 8
+    },
+    {
+        id: 3,
+        nombre: 'Carolina',
+        nota: 14
+    },
+    {
+        id: 4,
+        nombre: 'Wendy',
+        nota: 16
+    },
+    {
+        id: 5,
+        nombre: 'Andrea',
+        nota: 19
+    },
+    {
+        id: 6,
+        nombre: 'Pamela',
+        nota: 19
+    },
+    {
+        id: 7,
+        nombre: 'Cristian',
+        nota: 20
+    },
+    {
+        id: 8,
+        nombre: 'Daniel',
+        nota: 19
+    },
+    {
+        id: 9,
+        nombre: 'Lilly',
+        nota: 14
+    },
+    {
+        id: 10,
+        nombre: 'Ramiro',
+        nota: 12
+    }
+];
+
+//Operadores: Funciones como parámetros
+//find
+//enviamos una expresión -> Truty Falsy
+//devuelve el primero que cumpla esa condición
+const respuestaFind  = arreglo.find(
+    function (valorActual, indiceActual, arregloCompleto){
+        console.log('valorActual', valorActual);
+        console.log('indiceActual', indiceActual);
+        console.log('arregloCompleto', arregloCompleto);
+        return valorActual.nombre === "Cristian"; //EXPRESION ===
+    }
+);
+
+console.log('respuestaFind', respuestaFind) //Resp: Cristian; si no encuentra devuelve undefined
