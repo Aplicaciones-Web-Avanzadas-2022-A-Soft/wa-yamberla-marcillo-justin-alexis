e486218d1c95e6d2192d9831470f023ae59f0d40
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-05-18 08:26:28 -0500

    Operadores 2

--- 01-javascript/03-operadores.js
+++ 01-javascript/03-operadores.js
@@ -77,3 +77,77 @@ const respuestaIndex  = arreglo.findIndex(
 );
 
 console.log('respuestaIndex', respuestaIndex) //Resp: indice 6; si no encuentra devuelve -1
+
+//foreach: itera el arreglo
+const respuestaForEach = arreglo.forEach(
+    function (valorActual, indiceActual, arregloCompleto){
+        console.log('ValorActual', valorActual);
+    }
+);
+console.log('respuestaForEach', respuestaForEach); //undefined
+
+//MAP: Modificar o mutar el arreglo y devuelve un nuevo arreglo
+//enviamos datos del nuevo arreglo
+//devuelve el nuevo arreglo
+
+const respuestaMap = arreglo.map(
+    (valorActual, indiceActual, arregloCompleto)=>{
+        const nuevoElemento = {
+            id: valorActual.id,
+            nombre: valorActual.nombre,
+            nota: valorActual.nota + 1,
+            casado: false
+        };
+        return nuevoElemento;
+    }
+);
+console.log('respuestaMap', respuestaMap);
+console.log('arreglo', arreglo);
+
+//Filter: filtrar el arreglo
+//enviamos una expresión truty o falsy
+//devuelve los elementos que cumplen esa condición
+const respuestaFilter = arreglo.filter(
+    (valorActual, indiceActual, arregloCompleto) =>{
+        return valorActual.nota >= 14;
+    }
+);
+console.log('respuestaFilter', respuestaFilter);
+console.log('arreglo', arreglo);
+
+//some: pasamos expresión truty o falsy
+//devuelve boolean
+//Hay alguna nota menor a nueve? SI o NO
+//OR
+const respuestaSome = arreglo.some(
+    function (valorActual, indiceActual, arregloCompleto){
+        return valorActual.nota < 9;
+    }
+);
+console.log('respuestaSome', respuestaSome);
+
+//every: pasamos expresión truty o falsy
+//devuelve boolean
+//Todas las notas son mayores a 14? SI o NO
+//AND
+const respuestaEvery = arreglo.every(
+    function (valorActual, indiceActual, arregloCompleto){
+        return valorActual.nota > 14;
+    }
+);
+console.log('respuestaEvery', respuestaEvery);
+
+//reduce: izq -> der
+//reduce right: der -> izq
+//100 < 3 ptos vida
+//100 -1 -2 -3 -5 -6 -5 -4 -3 -1 =
+// 0 1 +2 +3 +4 +5 +6 +5 +4 +3 +1 =
+// [1, 2, 3, 5, 6, 5, 4 , 3, 1]
+const respuestaReduce = arreglo.reduce(
+    function (valorAcumulado, valorActual, arreglo){
+        return (valorAcumulado + valorActual.nota);
+    },
+    0 //Acumulador
+);
+console.log('respuestaReduce', respuestaReduce); //100 - X = -46
+console.log('respuestaReduce', respuestaReduce / arreglo.length); //promedio
