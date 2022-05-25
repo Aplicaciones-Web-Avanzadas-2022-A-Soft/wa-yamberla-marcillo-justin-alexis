4a4c743888dfb5be9b5834e344139dadec66b1da
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-05-25 07:29:49 -0500

    Ejercicio Clase 08
    Ejercicio callbacks, lectura de archivos

--- 01-javascript/06-callbacks.js
+++ 01-javascript/06-callbacks.js
@@ -2,7 +2,7 @@ const fs = require('fs'); //file system
 
 //06-ejemplo.txt -> Hola
 
-console.log('PRIMERO');
+/*console.log('PRIMERO');
 fs.readFile(
     './06-ejemplo.txt', //1
     'utf-8',
@@ -11,4 +11,24 @@ fs.readFile(
     }
 );
 
-console.log('TERCERO')
+console.log('TERCERO')*/
+
+// EJERCICIO
+//1) Leer archivo: 06-ejemplo.txt, luego imprimir en consola
+// 2) Después del paso 1, Leer archivo: 01-variables.js, luego imprimir en consola
+
+fs.readFile(
+    './06-ejemplo.txt', //1
+    'utf-8',
+    (error, contenido) => {
+        console.log(contenido);
+    }
+);
+
+fs.readFile(
+    './01-variables.js', //2
+    'utf-8',
+    (error, contenido) => {
+        console.log(contenido);
+    }
+);
