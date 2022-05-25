8dd23d313288e1036f829041bdd988365797c880
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-05-25 08:49:53 -0500

    CLase 08
    Ejercicio Lectura/Escritura, Promesas

--- 01-javascript/06-callbacks.js
+++ 01-javascript/06-callbacks.js
@@ -1,11 +1,12 @@
-const fs = require('fs'); //file system
+const fs = require('fs'); //file system: Importar módulo
 
 //06-ejemplo.txt -> Hola
 
 /*console.log('PRIMERO');
+
 fs.readFile(
-    './06-ejemplo.txt', //1
-    'utf-8',
+    './06-ejemplo.txt', //Nombre del archivo
+    'utf-8',            //Codificación
     (error, contenido) => {
         console.log('SEGUNDO');
     }
@@ -14,21 +15,68 @@ fs.readFile(
 console.log('TERCERO')*/
 
 // EJERCICIO
-//1) Leer archivo: 06-ejemplo.txt, luego imprimir en consola
+// Link info fs: https://nodejs.dev/learn/the-nodejs-fs-module
+// 1) Leer archivo: 06-ejemplo.txt, luego imprimir en consola
 // 2) Después del paso 1, Leer archivo: 01-variables.js, luego imprimir en consola
-
+/*
 fs.readFile(
     './06-ejemplo.txt', //1
     'utf-8',
-    (error, contenido) => {
-        console.log(contenido);
+    (errorLectura1, contenidoPrimerArchivo) => {
+        if(errorLectura1){
+            console.log('Error leyendo el primer archivo');
+            throw new Error('ERROR LEYENDO PRIMER ARCHIVO');
+        }else {
+            fs.readFile(
+                './01-variables.js', //2
+                'utf-8',
+                (errorLectura2, contenidoSegundoArchivo) => {
+                    if(errorLectura2){
+                        console.log('Error leyendo el segundo archivo');
+                        throw new Error('ERROR LEYENDO SEGUNDO ARCHIVO');
+                    }else {
+                        console.log(contenidoPrimerArchivo, contenidoSegundoArchivo);
+                    }
+                }
+            );
+        }
+    }
+);
+*/
+
+// 3) Crear un nuevo archivo llamado 06-nuevo-archivo.txt con el contenido de los otros dos archivos.
+
+/* fs.writeFile(
+    'nombreArchivo()Path',
+    'contenidoArchivo',
+    (errorEscritura)=>{
+
+    }
+); */
+
+fs.writeFile(
+    './06-nuevo-archivo.txt',
+    contenidoArchivo,
+    (errorEscritura)=>{
+        if (errorEscritura) {
+            console.error('Error de escritura');
+        }else {
+            console.log('Archivo escrito correctamente');
+        }
     }
 );
 
+/*
 fs.readFile(
-    './01-variables.js', //2
-    'utf-8',
+    './06-ejemplo.txt', //Nombre del archivo
+    'utf-8',            //Codificación
     (error, contenido) => {
-        console.log(contenido);
+        if(error){
+            console.log('Error leyendo el archivo');
+            throw new Error('ERROR LEYENDO ARCHIVO');
+        }else {
+            console.log('Archivo leído correctamente');
+        }
     }
-);
+),
+*/
---
+++ 01-javascript/07-promesas.js
@@ -0,0 +1,38 @@
+//07-promesas.js
+const fs = require('fs');
+
+//envolver dentro de una función
+
+function promesaEsPar(numero){
+    const miPrimeraPromesa = new Promise( //Definición de la promesa
+        (
+            resolve, //funcion return
+            reject   //funcio  throw
+        ) => {
+            if (numero % 2 === 0){
+                resolve(numero);  //return numerop -> resolve() : return undefined
+            }else {
+                reject('No es par :(')  //Throw: 'No es par :('
+            }
+
+        }
+    )
+    return miPrimeraPromesa
+}
+
+promesaEsPar(20)
+.then( //return
+    (respuesta) =>{
+        console.log("Respuesta: ", respuesta)
+    }
+)
+.catch( //throw
+    (error) =>{
+        console.error('Error: ', error)
+    }
+)
+.finally( //finally
+    ()=>{
+        console.info('Fin');
+    }
+)
