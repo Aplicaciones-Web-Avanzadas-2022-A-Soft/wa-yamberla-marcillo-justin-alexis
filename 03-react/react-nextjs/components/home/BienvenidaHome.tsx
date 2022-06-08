125d430e49a02751a25a87205388f0e4c2c55f95
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-06-08 09:02:36 -0500

    Clase 14

---
+++ 03-react/react-nextjs/components/home/BienvenidaHome.tsx
@@ -0,0 +1,7 @@
+const BienvenidaHome = () =>(
+    <>
+        <h1>Bienvenida Home</h1>
+    </>
+)
+
+export default BienvenidaHome
--- 03-react/react-nextjs/pages/mi-perfil.tsx
+++ 03-react/react-nextjs/pages/mi-perfil.tsx
@@ -1,4 +1,6 @@
 import Layout from "../components/Layout";
+import BienvenidaHome from "../components/home/BienvenidaHome";
+import LoginHome from "../components/home/LoginHome";
 
 const MiPerfilPage = () => {
 
@@ -6,20 +8,86 @@ const MiPerfilPage = () => {
     const listaNumeros = arreglo
         .filter((a) => a < 5)
         .map((a) => {
-            return(
+            return (
                 <li>
                     {a}
                 </li>
             )
-    })
+        })
+
+    /*function mostrarLogin(){
+        return (
+            <>
+                <h1> Login </h1>
+                <p>Campos Login</p>
+            </>
+        )
+    }
+    function mostrarMensajeBienvenida(){
+        return (
+            <>
+                <h1> Bienvenido </h1>
+                <p>Registrate por favor</p>
+            </>
+        )
+    }*/
+
+    const estaLogeado = true;
+
+    const desplegarMensaje = () => {
+        if (estaLogeado) {
+            return LoginHome({
+                propiedadesImagen: {
+                    width: 400,
+                    height: 200,
+                     urlImagen: 'https://i.ytimg.com/vi/euXlgrP5QiE/maxresdefault.jpg'
+                }
+            })
+        } else {
+            return BienvenidaHome()
+        }
+    }
+
     return (
         <Layout>
             <p>Hello {'Justin'.toUpperCase()}</p>
             <ul>
                 {listaNumeros}
             </ul>
+            {/*<p> CONDICION ? VERDADERA : FALSA </p>*/}
+            {/*<p>{estaLogeado ? 'Bienvenido' : 'Login'}</p>*/}
+            {/*<p>{estaLogeado ? mostrarLogin(): mostrarMensajeBienvenida()}</p>*/}
+            <p>{desplegarMensaje()}</p>
+
+            <ul>
+                {(arreglo.length > 0) &&
+                    <>
+                        <li>Hola amigos</li>
+                    </>
+                }
+            </ul>
+
+            <ul>
+                {(arreglo.length > 10) &&
+                    listaNumeros
+                }
+            </ul>
+
+            <div>
+                {estaLogeado?
+                    <LoginHome propiedadesImagen={
+                        {
+                        width: 300,
+                        height: 150,
+                        urlImagen: 'https://i.ytimg.com/vi/euXlgrP5QiE/maxresdefault.jpg'
+                        }
+                    }></LoginHome>:
+                    <BienvenidaHome></BienvenidaHome>
+                }
+            </div>
+
         </Layout>
     )
 }
 
-export default  MiPerfilPage
+export default MiPerfilPage
