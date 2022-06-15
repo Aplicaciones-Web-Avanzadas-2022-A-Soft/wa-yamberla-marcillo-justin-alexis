32c22eb738163d43f9ce925ac89beb5432f794d3
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-06-14 22:25:53 -0500

    Deber 02
    Entidades v1.0

--- Deberes/02-Deber-Entidades/02-deberEntidades.js
+++ Deberes/02-Deber-Entidades/02-deberEntidades.js
@@ -1,66 +1,177 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
 
 let autor = {
-    nombre: 'John',
-    apellido: 'Katzenbach',
+    nombre: 'John Katzenbach',
+    direccion: undefined,
     edad: 71,
     hijos: null,
     casado: true,
     libros: []
 };
 
-let libro;
+let libro = {
+    id: 0,
+    titulo: '',
+    fechaPublicacion: '',
+    numeroPaginas: 0,
+    genero: '',
+};
 
-//funcion para eliminar al autor
-const eliminarAutor = () =>{
-    autor = {}
-    eliminarLibro();
-}
-
-//funcion para crear el libro
+//------------------------ CREATE --------------------------------
 const crearLibro = () =>{
     inquirer.prompt([
         {
+            name: 'id',
+            type: 'input',
+            message: 'Ingrese el id: '
+        },
+        {
             name: 'titulo',
             type: 'input',
-            message: 'Ingrese el titulo:'
+            message: 'Ingrese el titulo: '
+        },
+        {
+            name: 'fechaPublicacion',
+            type: 'input',
+            message: 'Ingrese la fecha de publicación: '
+        },
+        {
+            name: 'numPaginas',
+            type: 'input',
+            message: 'Ingrese el número de páginas: '
+        },
+        {
+            name: 'genero',
+            type: 'input',
+            message: 'Ingrese el género: '
         },
     ]).then((respuestas) => {
-        autor.libros = respuestas.titulo;
+        libro.id = respuestas.id
+        libro.titulo = respuestas.titulo
+        libro.fechaPublicacion = respuestas.fechaPublicacion
+        libro.numeroPaginas = respuestas.numPaginas
+        libro.genero = respuestas.genero
+
+        autor.libros.push(libro)
+        let objetoGuardado = JSON.stringify(autor)
+        fs.writeFile('./data.txt', objetoGuardado, (error) =>{
+            if(error){
+                console.log('Error')
+            }else{
+                console.log('Archivo generado correctamente')
+            }
+        })
+
+        console.log(autor)
     });
 }
-//funcion para eliminar el libro
-const eliminarLibro = () =>{
-    libro = {
-        titulo: '',
-        genero: '',
-        numeroPaginas: '',
-        editorial: '',
-        fechaPublicacion: ''
-    }
+
+//------------------------- READ ---------------------------------
+const leerLibro = () =>{
+    let datosArchivo = fs.readFileSync('./data.txt')
+    let datosAutor = JSON.parse(datosArchivo)
+    console.log(datosAutor)
+}
+//------------------------- UPDATE -------------------------------
+const actualizarLibro = () =>{
+    inquirer.prompt([
+        {
+            name: 'id',
+            type: 'input',
+            message: 'Ingrese el nuevo id: '
+        },
+        {
+            name: 'titulo',
+            type: 'input',
+            message: 'Ingrese el nuevo titulo: '
+        },
+        {
+            name: 'fechaPublicacion',
+            type: 'input',
+            message: 'Ingrese la nueva fecha de publicación: '
+        },
+        {
+            name: 'numPaginas',
+            type: 'input',
+            message: 'Ingrese el nuevo número de páginas: '
+        },
+        {
+            name: 'genero',
+            type: 'input',
+            message: 'Ingrese el nuevo género: '
+        },
+    ]).then((respuestas) => {
+        let datosArchivo = fs.readFileSync('./data.txt')
+        let datosAutor = JSON.parse(datosArchivo)
+
+        libro.id = respuestas.id
+        libro.titulo = respuestas.titulo
+        libro.fechaPublicacion = respuestas.fechaPublicacion
+        libro.numeroPaginas = respuestas.numPaginas
+        libro.genero = respuestas.genero
+        datosAutor.libros = libro
+
+        let objetoGuardado = JSON.stringify(datosAutor)
+        fs.writeFile('./data.txt', objetoGuardado, (error) =>{
+            if(error){
+                console.log('Error')
+            }else{
+                console.log('Archivo actualizado correctamente')
+            }
+        })
+        console.log(datosAutor)
+    });
 }
 
-//menu
+//------------------------- DELETE -------------------------------
+const eliminarLibro = () =>{
+    libro = {}
+    autor.libros = libro
+    let objetoGuardado = JSON.stringify(autor)
+    fs.writeFile('./data.txt', objetoGuardado, (error) =>{
+        if(error){
+            console.log('Error')
+        }else{
+            console.log('Libro eliminado correctamente')
+        }
+    })
+}
+
+//------------------------ MENU -----------------------------
 const menu = () =>{
     inquirer.prompt([
         {
             name: 'opcion',
             type: 'list',
             message: '¿Qué desea hacer?',
-            choices: ['1. Crear libro', '2. Leer libro', '3. Salir']
+            choices: [
+                '1. Crear libro',
+                '2. Leer libro',
+                '3. Actualizar Libro',
+                '4. Eliminar libro',
+                '5. Salir']
         },
     ]).then((respuesta) =>{
         switch (respuesta.opcion){
             case '1. Crear libro':
                 crearLibro()
-                menuSalida()
-            break;
-            case '2. Leer libro': console.log('Leyendo...')
-            break;
+                break;
+            case '2. Leer libro':
+                leerLibro()
+                break;
+            case '3. Actualizar Libro':
+                actualizarLibro()
+                break;
+            case '4. Eliminar libro':
+                eliminarLibro()
+                break;
+            case '5. Salir':
+                console.log('Saliendo')
         }
     });
 }
-
+/*
 const menuSalida = () =>{
     inquirer.prompt([
         {
@@ -72,9 +183,9 @@ const menuSalida = () =>{
         if(respuesta.opcionSalida){
             menu();
         }else {
-            console.log(autor);
+            console.log('ADIOS');
         }
     });
-}
+}*/
 
 menu();
