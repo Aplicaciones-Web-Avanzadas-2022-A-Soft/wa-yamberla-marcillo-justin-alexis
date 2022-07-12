7e9746643da6b863d238c1ffdc82fc0f3d217eb7
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-07-12 08:56:59 -0500

    Actualización Clase 22-23

--- 03-react/react-nextjs/components/Layout.tsx
+++ 03-react/react-nextjs/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import {Toaster} from "react-hot-toast";
 
 type Props = {
   children?: ReactNode
@@ -15,7 +16,17 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-              crossOrigin="anonymous"></link>
+              crossOrigin="anonymous">
+        </link>
+      <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+
     </Head>
     <header>
       <nav>
@@ -60,6 +71,10 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <hr />
       <span>I'm here to stay (Footer)</span>
     </footer>
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+    />
   </div>
 )
 
--- 03-react/react-nextjs/pages/formulario.tsx
+++ 03-react/react-nextjs/pages/formulario.tsx
@@ -1,19 +1,128 @@
 import Layout from "../components/Layout";
+import {useState} from "react";
+import {useForm, Controller } from "react-hook-form";
+import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {toast} from "react-hot-toast";
 
-export default function Formulario(){
-    return(
+type FormularioEjemplo = {
+    nombre: string;
+    estadoCivil: string;
+}
+
+export default function Formulario() {
+    const [nombre, setNombre] = useState('Justin');
+    // const [numuno, numdos] = [ 1,2]; // Importa el orden
+    // const {a} = {a:1,b:2,c:3} as any; // importa el nombre del objeto
+
+    const {control, register, handleSubmit, formState: {errors, isValid}} = useForm<FormularioEjemplo>(
+        {
+            defaultValues: {
+                nombre: 'Justin',
+                estadoCivil: '',
+            },
+            mode: 'onTouched'
+        }
+    )
+    const controlarSubmit = (eventoSubmit) => {
+        eventoSubmit.preventDefault();
+        console.log('Submit', eventoSubmit);
+        console.log('nombre', nombre);
+    }
+    const controlarSubmitRHF = (data) => {
+        console.log('data', data);
+        toast('Good job!', {icon: '🦍'});
+        toast.success('Bien!')
+        toast.success('Mal!')
+    }
+    return (
         <>
-            <Layout title={"Formulario"}>
-                <form>
+            <Layout title="Formulario">
+                <h1>Formulario con react Hook Form</h1>
+                <form onSubmit={handleSubmit(controlarSubmitRHF)}>
                     <div className="mb-3">
-                        <label htmlFor="nombre" className="form-label"> Nombre </label>
+                        <label htmlFor="nombre" className="form-label">Nombre</label>
                         <input type="text"
-                            className="form-control"
-                            placeholder="Ej: Justin"
-                            id="nombre"
-                            aria-describedby="nombreHelp"/>
+                               className="form-control"
+                               placeholder="EJ: Adrian"
+                               id="nombre"
+                               {...register('nombre', {
+                                   maxLength: {value: 20, message: 'Longitud maxima 20'},
+                                   required: {value: true, message: 'Requerido'},
+                                   minLength: {value: 5, message: 'Longitud minima 5'},
+                                   validate: {
+                                       soloNumeros: (valorActual) => {
+                                           // Transformar a numero un string:
+                                           // Number("1")
+                                           // +"1"
+                                           if (Number.isNaN(+valorActual)) {
+                                               // Se puede devolver un false o un mensaje de error
+                                               // return false; // Error
+                                               return 'Ingrese solo numeros'; // Error
+                                           } else {
+                                               // Se devuelve un true
+                                               return true; // Esta correcto
+                                           }
+                                       },
+                                       // soloLetras: (valorActual) => {
+                                       //     return 'Error de prueba';
+                                       // },
+                                   }
+                               })}
+                               aria-describedby="nombreHelp"/>
                         <div id="nombreHelp" className="form-text">
-                            INGRESA TU NOMBRE
+                            Ingresa tu nombre.
+                        </div>
+                        {errors.nombre &&
+                            <div className="alert alert-warning" role="alert">
+                                Tiene errores {errors.nombre.message}
+                            </div>
+                        }
+                    </div>
+                    <div className="mb-3">
+                        <FormControl fullWidth>
+                            <InputLabel id="estadoCivilLabelId">Estado Civil</InputLabel>
+                            <Controller
+                                control={control}
+                                rules={{ required: {value: true, message: 'Estado civil requerido!'}}}
+                                name="estadoCivil"
+                                render={({ field: { onChange, value, onBlur,}}) => {
+                                    return <Select
+                                        labelId="estadoCivilLabelId"
+                                        id="estadoCivilId"
+                                        onBlur={onBlur}
+                                        value={value}
+                                        label="Estado Civil"
+                                        onChange={onChange}
+                                    >
+                                        <MenuItem value={''}>Seleccione uno</MenuItem>
+                                        <MenuItem value={'casado'}>Casado</MenuItem>
+                                        <MenuItem value={'soltero'}>Soltero</MenuItem>
+                                    </Select>
+                                }}
+                            />
+                        </FormControl>
+                    </div>
+                    <button type="submit"
+                            disabled={!isValid}
+                            className="btn btn-primary">
+                        Submit
+                    </button>
+                </form>
+                <br/>
+                <h1>Formulario con React</h1>
+                <form onSubmit={controlarSubmit}>
+                    {/*<form onSubmit={(e)=>controlarSubmit(e)}>*/}
+                    <div className="mb-3">
+                        <label htmlFor="nombre" className="form-label">Nombre</label>
+                        <input type="text"
+                               className="form-control"
+                               onChange={(e) => setNombre(e.target.value)}
+                               placeholder="EJ: Justin"
+                               value={nombre}
+                               id="nombre"
+                               aria-describedby="nombreHelp"/>
+                        <div id="nombreHelp" className="form-text">
+                            Ingresa tu nombre.
                         </div>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
