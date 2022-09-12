d3f55557e2c39c3a164f5766c05b54046d4c39ee
Justin Yamberla <justin.yamberla@epn.edu.ec>
2022-09-11 21:38:20 -0500

    Actualización Examen Final

--- ExamenB1Final/autores-app/pages/index.jsx
+++ ExamenB1Final/autores-app/pages/index.jsx
@@ -5,8 +5,8 @@ export default Home;
 function Home() {
     return (
         <div>
-            <h1>Examen 1 App Web Avanzadas</h1>
-            <p>Examen de Primer Bimestre de Aplicaciones Web Avanzadas</p>
+            <h1>Examen 2 App Web Avanzadas</h1>
+            <p>Examen de Segundo Bimestre de Aplicaciones Web Avanzadas</p>
             <p><Link href="/autores"> Gestionar Autores </Link></p>
         </div>
     );
